9b6db9b40054ed5d2f832d801dd1187068868b12
dipankarshaw <dipcom02323@gmail.com>
2020-05-01 19:41:30 +0530

    Loop deleted for Policy & CCM chnaged to 1s, X connect Scalled to 200.

--- BD_Scale.sh
+++ BD_Scale.sh
@@ -106,7 +106,7 @@ ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":1,"FF_end_evi_ID":50,
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":100,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags create_xc_service
 a=100
 b=200
- until [ $a -ge 200 ]
+ until [ $a -ge 300 ]
  do
  ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"Flat_QOS":"YES","CCM_REQUIRED":"y","DM_SL_REQUIRED":"YES"}' --tags create_xc_service
  a=`expr $a + 100`
@@ -120,7 +120,7 @@ ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":1,"FF_end_evi_ID":50,
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":100,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service
 a=100
 b=200
- until [ $a -ge 200 ]
+ until [ $a -ge 300 ]
  do
  ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"QOS_REQUIRED":'YES',"Flat_QOS":"YES","CCM_REQUIRED":"y","DM_SL_REQUIRED":"YES"}' --tags delete_xc_service
  a=`expr $a + 100`
