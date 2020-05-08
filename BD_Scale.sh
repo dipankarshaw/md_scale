2cb3590c13c5a8a3144ae7f90a0adf4def048217
dipankarshaw <dipcom02323@gmail.com>
2020-05-08 20:14:02 +0530

    duration_improve

--- BD_Scale.sh
+++ BD_Scale.sh
@@ -23,7 +23,7 @@ a=500
 b=550
  until [ $a -ge 900 ]
  do
- ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"Flat_QOS":"YES","CCM_REQUIRED":"y","DM_SL_REQUIRED":"YES"}' --tags create_xc_service_CORE1
+ ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags create_xc_service_CORE1
  a=`expr $a + $step`
  b=`expr $b + $step` 
  done
@@ -43,7 +43,7 @@ cd ..
 ls -lrt
 
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":1,"FF_end_evi_ID":50,"H_QOS":"YES","Flat_QOS":"NO","CCM_REQUIRED":"n"}' --tags delete_ELAN_service
-ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":100,"Flat_QOS":"YES","CCM_REQUIRED":"y","DM_SL_REQUIRED":"YES"}' --tags delete_xc_service
+ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":100,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service
 a=100
 b=150
  until [ $a -ge 400 ]
@@ -57,7 +57,7 @@ a=500
 b=550
  until [ $a -ge 900 ]
  do
- ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"QOS_REQUIRED":'YES',"Flat_QOS":"YES","CCM_REQUIRED":"y","DM_SL_REQUIRED":"YES"}' --tags delete_xc_service_CORE1
+ ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"QOS_REQUIRED":'YES',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service_CORE1
  a=`expr $a + $step`
  b=`expr $b + $step` 
  done
