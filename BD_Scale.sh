757fb2a7aa95eae3180cc36366fe4a262c593113
dipankarshaw <dipcom02323@gmail.com>
2020-05-13 20:02:37 +0530

    Removed loop

--- BD_Scale.sh
+++ BD_Scale.sh
@@ -4,68 +4,39 @@ echo "
 Multi-Dimension test Case
 ***********
 "
-step=50
+# Configuration of services
 ansible-playbook xconnect_scale.yml --tags verify_service
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":1,"FF_end_evi_ID":2}' --tags configure_policy
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":1,"FF_end_evi_ID":50,"H_QOS":"YES","Flat_QOS":"NO","CCM_REQUIRED":"n"}' --tags create_ELAN_service
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":150,"Flat_QOS":"YES","CCM_REQUIRED":"y","DM_SL_REQUIRED":"YES"}' --tags create_xc_service
-a=150
-b=200
- until [ $a -ge 400 ]
- do
- ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags create_xc_service
- a=`expr $a + $step`
- b=`expr $b + $step` 
- done
-
-a=500
-b=550
- until [ $a -ge 900 ]
- do
- ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags create_xc_service_CORE1
- a=`expr $a + $step`
- b=`expr $b + $step` 
- done
-
-echo "
-*********
-Adding a pause for 6 minutes to collect DM/SL stats
-*********
-"
+ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":150,"FF_end_evi_ID":420,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags create_xc_service
+ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":500,"FF_end_evi_ID":700,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags create_xc_service_CORE1
+ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":700,"FF_end_evi_ID":920,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags create_xc_service_CORE1
+echo " ********* Adding a pause for 6 minutes to collect DM/SL stats ********* "
+# Verification of services
 sleep 6m
 ansible-playbook xconnect_scale.yml --tags verify_service
 ansible-playbook gather.yml
-ansible-playbook verifier.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":400}' --tags service_verify_core0
-ansible-playbook verifier.yml -e '{"FF_start_evi_ID":500,"FF_end_evi_ID":900}' --tags service_verify_core1
-ansible-playbook verifier.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":400}' --tags ccm_verify
-ansible-playbook verifier.yml -e '{"FF_start_evi_ID":500,"FF_end_evi_ID":900}' --tags ccm_verify
+echo " ********* Check the status of all X-connect services on Core 0 ********* "
+ansible-playbook verifier.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":420}' --tags service_verify_core0
+echo " ********* Check the status of all X-connect services on Core 1 ********* "
+ansible-playbook verifier.yml -e '{"FF_start_evi_ID":500,"FF_end_evi_ID":920}' --tags service_verify_core1
+echo " ********* Check CCM status of all X-connect services on Core 0 ********* "
+ansible-playbook verifier.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":420}' --tags ccm_verify
+echo " ********* Check CCM status of all X-connect services on Core 1 ********* "
+ansible-playbook verifier.yml -e '{"FF_start_evi_ID":500,"FF_end_evi_ID":920}' --tags ccm_verify
+echo " ********* Check DM stats of 100 services on Core 0 ********* "
 ansible-playbook verifier.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":150}' --tags dmm_verify
+echo " ********* Check DM stats of 100 services on Core 0 ********* "
 ansible-playbook verifier.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":150}' --tags slm_verify
-ls -lrt
+# Spirent Traffic verification
 chmod 777 spirent_traffic/
-ls -lrt
 cd spirent_traffic/
-ls -lrt
 python service_traffic_400.py
-ls -lrt
 cd ..
-ls -lrt
+#Deletion of services.
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":1,"FF_end_evi_ID":50,"H_QOS":"YES","Flat_QOS":"NO","CCM_REQUIRED":"n"}' --tags delete_ELAN_service
 ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":50,"FF_end_evi_ID":150,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service
-a=150
-b=200
- until [ $a -ge 400 ]
- do
- ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"QOS_REQUIRED":'YES',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service
- a=`expr $a + $step`
- b=`expr $b + $step` 
- done
-
-a=500
-b=550
- until [ $a -ge 900 ]
- do
- ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b',"QOS_REQUIRED":'YES',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service_CORE1
- a=`expr $a + $step`
- b=`expr $b + $step` 
- done
+ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":150,"FF_end_evi_ID":420,"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service
+ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":500,"FF_end_evi_ID":700,"QOS_REQUIRED":'YES',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service_CORE1
+ansible-playbook xconnect_scale.yml -e '{"FF_start_evi_ID":700,"FF_end_evi_ID":920,"QOS_REQUIRED":'YES',"Flat_QOS":"YES","CCM_REQUIRED":"y"}' --tags delete_xc_service_CORE1
